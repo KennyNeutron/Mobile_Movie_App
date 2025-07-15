844220a40f0e69542c0fa3d443c339ecc8ba5f14
KennyNeutron <kennyneutron.hex@gmail.com>
2025-07-16 04:57:04 +0800

    setup expo and nativewind

---
+++ app/index.tsx
@@ -0,0 +1,10 @@
+import { Text, View } from "react-native";
+import "../global.css"; // Import global styles
+
+export default function App() {
+  return (
+    <View className="flex-1 items-center justify-center">
+      <Text className="text-5xl text-primary font-bold">Hello World!</Text>
+    </View>
+  );
+}
