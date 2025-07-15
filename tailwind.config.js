fc8a53f80429e643f521752803efda81105f128d
KennyNeutron <kennyneutron.hex@gmail.com>
2025-07-16 05:48:49 +0800

    fixed errors on tailwindconfig.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  // NOTE: Update this to include the paths to all files that contain Nativewind classes.
-  content: ["./App.tsx", "./components/**/*.{js,jsx,ts,tsx}"],
+  // NOTE: Update this to include the paths to all of your component files.
+  content: ["./app/**/*.{js,jsx,ts,tsx}"],
   presets: [require("nativewind/preset")],
   theme: {
-    extend: {
-      colors: {
-        primary: "#030014",
-      },
-    },
+    extend: {},
   },
   plugins: [],
 };
